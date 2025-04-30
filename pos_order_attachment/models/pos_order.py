97bd41c1d73d1be33edd7e302815cb582e2f8797
Vehiana Tepuhiarii <vehi@invitu.com>
2025-04-29 15:08:21 -1000

    [IMP][pos_order_attachment] Add filters for pos orders attachments

--- pos_order_attachment/models/pos_order.py
+++ pos_order_attachment/models/pos_order.py
@@ -1,12 +1,62 @@
 # Copyright 2024 Dixmit
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, models
+from odoo import api, fields, models
 
 
 class PosOrder(models.Model):
     _name = "pos.order"
     _inherit = ["pos.order", "mail.thread"]
+
+    has_attachments = fields.Boolean(
+        compute="_compute_has_attachments", search="_search_has_attachments"
+    )
+    has_no_attachments = fields.Boolean(
+        compute="_compute_has_attachments", search="_search_has_no_attachments"
+    )
+
+    def _compute_has_attachments(self):
+        __import__("pdb").set_trace()
+        # pour chaque enregistrement
+        for record in self:
+            # je vais chercher les attachments du model pos.order
+            # et que le res_id est l'id du record actuel
+            attachments = self.env["ir.attachment"].search_count(
+                [
+                    ("res_model", "=", "pos.order"),
+                    ("res_id", "=", record.id),
+                ]
+            )
+            # si le résultat de la recherche est supérieur à 0
+            if attachments > 0:
+                # renvoie vrai
+                record.has_attachments = True
+            # sinon
+            else:
+                # renvoie faux
+                record.has_attachments = False
+
+    def _search_has_attachments(self, operator, value):
+        orders = []
+        attachment_ids = self.env["ir.attachment"].search(
+            [
+                ("res_model", "=", "pos.order"),
+            ]
+        )
+        for attachment in attachment_ids:
+            orders.append(attachment.res_id)
+        return [("id", "in", orders)]
+
+    def _search_has_no_attachments(self, operator, value):
+        orders = []
+        attachment_ids = self.env["ir.attachment"].search(
+            [
+                ("res_model", "=", "pos.order"),
+            ]
+        )
+        for attachment in attachment_ids:
+            orders.append(attachment.res_id)
+        return [("id", "not in", orders)]
 
     @api.model_create_multi
     def create(self, vals_list):
